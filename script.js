428d7988bddea8255181e9500da7df74a9dc27f4
Thevren <167059653+Thevren@users.noreply.github.com>
2024-04-16 17:59:05 +1000

    Update script.js
    коментарии

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.body.innerHTML='<h1>Hello from JavaScript</h1>'
-console.log('Test 2')
+document.body.innerHTML='<h1>Hello from JavaScript</h1>' /* ставит как содержимое тела данный текст */
+console.log('Test 2') /* вывод теста */
